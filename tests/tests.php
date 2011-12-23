97daf4ace2b914fa8aa554f156ec5870e0551ac0
Gustavo <gustavosf@gmail.com>
2011-12-23 21:39:28 -0200

    escrito teste para a feature

--- tests/tests.php
+++ tests/tests.php
@@ -1,3 +1,18 @@
 <?php
 
-class PoaTransporteTestCase extends PHPUnit_Framework_TestCase {}
+class PoaTransporteTestCase extends PHPUnit_Framework_TestCase {
+	
+	public function testBusList()
+	{
+		$api = new PoaTransporte();
+		
+		$buses = $api->get_lists()->bus;
+		$this->assertEquals(gettype($buses), 'array');
+		
+		$this->assertEquals(gettype($bus->nome), 'object');
+		$this->assertEquals(get_class($bus->nome), 'PoaBusTransporte_Bus');
+		$this->assertObjectHasAttribute('nome', $bus);
+		$this->assertObjectHasAttribute('codigo', $bus);
+	}
+
+}
