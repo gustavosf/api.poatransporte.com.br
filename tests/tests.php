7a4e5ac6d59f43281d819bd129e4fb7cf02132d6
Gustavo <gustavosf@gmail.com>
2011-12-23 23:11:53 -0200

    Melhorado testes

--- tests/tests.php
+++ tests/tests.php
@@ -4,15 +4,23 @@ require dirname(__FILE__).'/../src/poatransporte.php';
 
 class PoaTransporteTestCase extends PHPUnit_Framework_TestCase {
 	
-	public function testBusList()
+	public function testBusCollection()
+	{
+		$buses = PoaTransporte::onibus();
+		$this->assertEquals(get_class($buses), 'PoaTransporte_Collection');
+		$this->assertGreatherThan(1, count($buses));
+
+		$bus = $buses[0];
+		$this->assertEquals(get_class($bus), 'PoaTransporte_Unit');
+	}
+
+	public function testBusUnitData()
 	{
 		$buses = PoaTransporte::onibus();
 		$bus = $buses[0];
 
-		$this->assertEquals(get_class($buses), 'PoaTransporte_Collection');
-		$this->assertEquals(get_class($bus), 'PoaTransporte_Unit');
-		$this->assertObjectHasAttribute('nome', $bus);
-		$this->assertObjectHasAttribute('codigo', $bus);
+		$this->assertNotNull(@$bus->nome);
+		$this->assertNotNull(@$bus->codigo);
 	}
 
 }
